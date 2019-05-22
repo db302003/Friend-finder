14a63760106d5262527a21ec1534037a3b55c6d8
Dawn Benton <46697755+db302003@users.noreply.github.com>
2019-05-22 01:23:10 -0500

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,125 @@
+var friends = [
+    
+	{
+	    name: "Rose",
+		photo: "https://goldengirls.fandom.com/wiki/Rose_Nylund?file=RoseNylund1.jpg",
+		scores: [
+
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1",
+            ]
+	},
+	{
+		name: "Blanche",
+		photo: "https://goldengirls.fandom.com/wiki/Blanche_Devereaux?file=Blanche.jpg",
+		scores: [
+
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            ]
+	},
+	{
+		name: "Dorothy",
+		photo: "https://goldengirls.fandom.com/wiki/Dorothy_Zbornak?file=Beaarthur_%281%29.jpg",
+		scores: [
+
+            "2",
+            "1",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "2",
+            "4",
+            "5",
+            ]
+	},
+	{
+		name: "Sophia",
+		photo: "https://goldengirls.fandom.com/wiki/Sophia_Petrillo?file=Sophia1.jpg",
+		scores: [
+
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5",
+            ]
+	},
+	{
+		name: "Stanley",
+		photo: "https://goldengirls.fandom.com/wiki/Stanley_Zbornak?file=ZMan.png",
+        scores: [
+
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3",
+            ]
+    },
+    {
+        name: "Dreyfus",
+		photo: "https://m.media-amazon.com/images/M/MV5BYmEwNjdlMWUtZjdkNi00Nzk5LWE5N2MtZjY0NWY5OWE0NDRiXkEyXkFqcGdeQXVyMTcyODY2NDQ@._V1_SY1000_CR0,0,1233,1000_AL_.jpg",
+        scores: [
+
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5",
+            ]
+    },
+    {   
+        name: "Dr. Harry Weston",
+		photo: "https://m.media-amazon.com/images/M/MV5BMzg3MTgxNDE2Ml5BMl5BanBnXkFtZTgwMDQ5MDY1MjE@._V1_.jpg",
+        scores: [
+
+            "2",
+            "2",
+            "1",
+            "4",
+            "3",
+            "5",
+            "2",
+            "3",
+            "1",
+            "4",
+        ]
+    }
+
+];
+
+module.exports = friends;
